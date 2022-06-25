897f4d00a2ac73d6dee3c87bcf23ef01559995b7
farisqadr <faris.code@gmail.com>
2022-06-25 21:29:30 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.bold, fontSize: 26);
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+    color: Color(0xff897B8), fontWeight: FontWeight.w300, fontSize: 16);
+TextStyle planTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.w500, fontSize: 14);
+TextStyle descTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.w300, fontSize: 12);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.w700, fontSize: 17);
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.w500, fontSize: 16);
